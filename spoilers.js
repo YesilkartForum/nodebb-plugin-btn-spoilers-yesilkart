3be379fcec28cec3cec520934292eba086e42e8b
Aydin 'devmach' Ulf <root@devmach.com>
2017-09-06 08:28:03 -0400

    Update button texts

--- spoilers.js
+++ spoilers.js
@@ -5,7 +5,7 @@ var YesilkartSpoilers = {
         // console.log(postData.postData.content)
         postData.postData.content = postData.postData.content
             .replace(/\#\#spoiler=([^<]*)/g, '***' + '$1' + '***' + '##spoiler')
-            .replace(/\#\#spoiler/g, '<div><div href="#" class="show-spoiler btn btn-md btn-default waves-effect" title="İçeriği Göster"><i class="fa fa-eye-slash fa-fw"></i><span class="btn-text" data-show_text="click to show" data-hide_text="click to hide">İçeriği Göster</span></div><div class="spoiler hidden">')
+            .replace(/\#\#spoiler/g, '<div><div href="#" class="show-spoiler btn btn-md btn-default waves-effect" title="İçeriği Göster"><i class="fa fa-eye-slash fa-fw"></i><span class="btn-text" data-show_text="İçeriği Göster" data-hide_text="İçeriği Gizle">İçeriği Göster</span></div><div class="spoiler hidden">')
             .replace(/\#\#endspoiler/g, '</div></div>')
 
         callback(null, postData);
